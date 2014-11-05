cfeca877f9cd67f07ab765301992383402a40ecd
Sho Hashimoto <hashimoto@shokai.org>
2014-11-06 08:57:32 +0900

    fix zsh history path

--- plugins/git.rb
+++ plugins/git.rb
@@ -6,7 +6,7 @@ day = Date.today-1
 h24 = Time.now.to_i-60*60*24
 cmds = Hash.new 0
 
-IO.popen("tail -5000 ~/.zsh-history", 'r+') do |io|
+IO.popen("tail -5000 ~/.zsh_history", 'r+') do |io|
   while line = io.gets
     line = line.toutf8
     time, cmd = line.scan(/: (\d+):\d+;(.+)/)[0]
